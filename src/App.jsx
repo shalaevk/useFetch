b3d9917453ad1c452f22a29d71be5d05ebdcb3dd
shalaevk <shalaevk1987@gmail.com>
2023-08-02 17:39:38 +0200

    feat: Add radio button for selecting API endpoint
    Add radio buttons to select between different API endpoints for fetching data in the App component.
    
    This commit implements the radio buttons and updates the URL for the fetch accordingly.
    
    Closes #123 (if applicable)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useFetch } from "./hooks";
+
+// If the API does not work use these local URLs
+// const URLS = {
+//   USERS: "users.json",
+//   POSTS: "posts.json",
+//   COMMENTS: "comments.json",
+// }
+
+const URLS = {
+  USERS: "https://jsonplaceholder.typicode.com/users",
+  POSTS: "https://jsonplaceholder.typicode.com/posts2",
+  COMMENTS: "https://jsonplaceholder.typicode.com/comments",
+};
+
+// // BONUS:
+// const OPTIONS = {
+//   method: "POST",
+//   body: JSON.stringify({ name: "Kyle" }),
+//   headers: {
+//     "Content-type": "application/json",
+//   },
+// };
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState(URLS.USERS);
+
+  const { data, isLoading, isError } = useFetch(url);
+
+  console.log("Data:", data, "Error:", isError, "iSLoading:", isLoading);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label>
+          <input
+            type="radio"
+            checked={url === URLS.USERS}
+            onChange={() => setUrl(URLS.USERS)}
+          />
+          Users
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={url === URLS.POSTS}
+            onChange={() => setUrl(URLS.POSTS)}
+          />
+          Posts
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={url === URLS.COMMENTS}
+            onChange={() => setUrl(URLS.COMMENTS)}
+          />
+          Comments
+        </label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : isError ? (
+        <h1>Error</h1>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
