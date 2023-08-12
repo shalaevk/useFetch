53d21b057589afd0cb4aabb5ba25cdd3f5a3c34f
shalaevk <shalaevk1987@gmail.com>
2023-08-12 15:17:09 +0200

    feat: Update App component with URL context and Child component
    - Import `createContext` from React
    - Import the `Child` component
    - Use a context provider to wrap the `Child` component
    - Display fetched data based on the URL context
    - Update radio button labels for different URL options

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import { useFetch } from "./hooks";
 import { Array } from "./Array";
 import { LocalStorage } from "../localStorage";
+import { Child } from "./Child";
 
 const URLS = {
   USERS: "https://jsonplaceholder.typicode.com/users",
-  POSTS: "https://jsonplaceholder.typicode.com/posts2",
+  POSTS: "https://jsonplaceholder.typicode.com/posts",
   COMMENTS: "https://jsonplaceholder.typicode.com/comments",
 };
 
@@ -18,47 +19,52 @@ const URLS = {
 //   },
 // };
 
+export const UrlContext = createContext("");
+
 function App() {
   const [url, setUrl] = useState(URLS.USERS);
+  const { state } = useFetch(url);
 
-  const { data, isLoading, isError } = useFetch(url);
   return (
     <>
-      {/* <div>
-        <label>
-          <input
-            type="radio"
-            checked={url === URLS.USERS}
-            onChange={() => setUrl(URLS.USERS)}
-          />
-          Users
-        </label>
-        <label>
-          <input
-            type="radio"
-            checked={url === URLS.POSTS}
-            onChange={() => setUrl(URLS.POSTS)}
-          />
-          Posts
-        </label>
-        <label>
-          <input
-            type="radio"
-            checked={url === URLS.COMMENTS}
-            onChange={() => setUrl(URLS.COMMENTS)}
-          />
-          Comments
-        </label>
-      </div>
-      {isLoading ? (
-        <h1>Loading...</h1>
-      ) : isError ? (
-        <h1>Error</h1>
-      ) : (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      )} */}
-      {/* <Array /> */}
-      <LocalStorage />
+      <UrlContext.Provider value={url}>
+        <Child />
+        <div>
+          <label>
+            <input
+              type="radio"
+              checked={url === URLS.USERS}
+              onChange={() => setUrl(URLS.USERS)}
+            />
+            Users
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={url === URLS.POSTS}
+              onChange={() => setUrl(URLS.POSTS)}
+            />
+            Posts
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={url === URLS.COMMENTS}
+              onChange={() => setUrl(URLS.COMMENTS)}
+            />
+            Comments
+          </label>
+        </div>
+        {state.isLoading ? (
+          <h1>Loading...</h1>
+        ) : state.isError ? (
+          <h1>Error</h1>
+        ) : (
+          <pre>{JSON.stringify(state.data, null, 2)}</pre>
+        )}
+        {/* <Array /> */}
+        {/* <LocalStorage /> */}
+      </UrlContext.Provider>
     </>
   );
 }
